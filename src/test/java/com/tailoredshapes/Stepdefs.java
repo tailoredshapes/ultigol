63f42db6a40ca90070fae3fbaf573a0a80616adc
Tom Marsh <tmarsh@tailoredshapes.com>
2018-08-03 16:34:32 -0400

    [TM YT] - Steps created and pending.

--- src/test/java/com/tailoredshapes/Stepdefs.java
+++ src/test/java/com/tailoredshapes/Stepdefs.java
@@ -4,5 +4,17 @@ import cucumber.api.PendingException;
 import cucumber.api.java8.En;
 
 public class Stepdefs implements En{
-    public Stepdefs() {}
+    public Stepdefs() {
+
+        Given("we have a game:", (String docString) -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+
+        Then("the cell {double} should be dead", (Double double1) -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+
+    }
 }
